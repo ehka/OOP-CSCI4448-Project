f5d3113a49af6bf5f0e28c7947614fbd2df87351
ehsankarimi93 <ehsan.karimi@colorado.edu>
2017-12-10 16:45:34 -0700

    code

--- src/poker/Player.java
+++ src/poker/Player.java
@@ -1,5 +1,24 @@
 package poker;
 
 public class Player {
-
+	public int score;
+	public String name;
+	public String[] friends;
+	public int chips;
+	
+	public void signup(String name, String password) {
+		
+	}
+	public void login(String name, String password) {
+		
+	}
+	public void add_friend(String name) {
+		
+	}
+	public int view_score() {
+		return 0;
+	}
+	public String[] view_friends(){
+		return friends;
+	}
 }
