62f5ae6941fbd0cb13927a946cd2da6448795e89
ehsankarimi93 <ehsan.karimi@colorado.edu>
2017-12-10 16:52:05 -0700

    code

---
+++ src/poker/Game.java
@@ -0,0 +1,19 @@
+package poker;
+
+public class Game {
+	public Player[] player_list;
+	public int[] board;
+	
+	public void create_game(Player[] player_list) {
+		
+	}
+	public void join_game(Player p) {
+		
+	}
+	public void leave_game(Player p) {
+		
+	}
+	public int[] check_board() {
+		return board;
+	}
+}
