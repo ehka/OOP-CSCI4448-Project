e21a0f3e475d6e8db1cda05ef12c5a64bd8e338a
ehsankarimi93 <ehsan.karimi@colorado.edu>
2017-12-10 17:05:48 -0700

    code

--- src/poker/Poker.java
+++ src/poker/Poker.java
@@ -1,5 +1,15 @@
 package poker;
 
 public class Poker {
-
+	public Cards[] cards;
+	public Cards[] seven_cards;
+	public int pot;
+	//observer here
+	
+	public void deal(Cards deal_cards) {
+		
+	}
+	public Cards[] flop(Cards flop_cards) {
+		return cards;
+	}
 }
